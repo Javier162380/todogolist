d5685df98e8d02f8d57a876b0128bf6f9e1c9679
javier162380 <javierllorente16@gmail.com>
2021-01-31 15:11:40 +0100

    adding api test to circleci

--- api/handler/create_test.go
+++ api/handler/create_test.go
@@ -17,17 +17,16 @@ func TestCreateTaskHandler(t *testing.T) {
 	repository := repository.NewRepository("memory", ".")
 
 	gin.SetMode(gin.TestMode)
-
 	r := gin.New()
-	r.POST("createtask/", CreateHandler(repository))
+	r.POST("/createtask", CreateHandler(repository))
 
 	t.Run("Invalid Request returns a 400", func(t *testing.T) {
-		createtaskrequests := taskRequest{TaskLabel: "work", TaskDate: "2020-12-12"}
+		createtaskrequests := taskRequest{TaskLabel: "work"}
 
 		b, err := json.Marshal(createtaskrequests)
 		require.NoError(t, err)
 
-		req, err := http.NewRequest("POST", "/creaatetask", bytes.NewBuffer(b))
+		req, err := http.NewRequest("POST", "/createtask", bytes.NewBuffer(b))
 		require.NoError(t, err)
 
 		rec := httptest.NewRecorder()
@@ -37,6 +36,25 @@ func TestCreateTaskHandler(t *testing.T) {
 		defer res.Body.Close()
 
 		assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+
+	})
+
+	t.Run("Correct Request returns a 200", func(t *testing.T) {
+		createtaskrequests := taskRequest{TaskLabel: "work", TaskName: "124"}
+
+		b, err := json.Marshal(createtaskrequests)
+		require.NoError(t, err)
+
+		req, err := http.NewRequest("POST", "/createtask", bytes.NewBuffer(b))
+		require.NoError(t, err)
+
+		rec := httptest.NewRecorder()
+		r.ServeHTTP(rec, req)
+
+		res := rec.Result()
+		defer res.Body.Close()
+
+		assert.Equal(t, http.StatusOK, res.StatusCode)
 
 	})
 
